ac32db572ae1468cf73e36f90cd0d62093e1362f
Johan Ekström <johan.ekstrom@live.se>
2021-10-11 21:17:53 +0200

    Styled. Missing animations

--- main.js
+++ main.js
@@ -2,23 +2,25 @@ let input = document.querySelector("#input");
 let btn = document.querySelector("#btn");
 let ulList = document.querySelector("#ulList");
 const myArray = [];
-let nrTasks = document.querySelector("#nrTasks");
+
 let warning = document.querySelector("#warning");
 let tasksCompleted = document.querySelector("#tasksCompleted")
 let completedCount = 0;
+let totalTasks = document.querySelector("#totalTasks");
 
 tasksCompleted.innerHTML = completedCount + " completed";
 //Knapp som lägger till punkter på ToDo-listan
-btn.addEventListener("click", function () {
+btn.addEventListener("click", function() {
 
     const text = input.value;
-    myArray.push(text);
+
 
     if (input.value == "") {
         warning.innerHTML = "Du måste ange text!";
         return;
     } else {
         warning.innerText = "";
+        myArray.push(text);
     }
 
 
@@ -30,7 +32,8 @@ btn.addEventListener("click", function () {
     itemLabel.innerHTML = text;
     newListItem.appendChild(itemLabel);
 
-    //newListItem.textContent = myArray[myArray.length -1];
+
+
 
     //nrTasks.innerHTML = myArray.length;
     console.log(ulList);
@@ -42,7 +45,7 @@ btn.addEventListener("click", function () {
     newListItem.appendChild(trashcan);
 
     //Lägg till klick på våra ToDos i listan
-    itemLabel.addEventListener("click", function () {
+    itemLabel.addEventListener("click", function() {
         if (itemLabel.getAttribute("class") == "completed") {
             itemLabel.setAttribute("class", "");
             completedCount--;
@@ -51,13 +54,14 @@ btn.addEventListener("click", function () {
             completedCount++;
         }
         //tasksCompleted.innerHTML = `${completedCount} completed`;
-        tasksCompleted.innerHTML = completedCount + " completed"
+        tasksCompleted.innerHTML = completedCount + " completed";
     });
 
     //Lägg till klick på vår trashcan
     trashcan.addEventListener("click", function() {
         newListItem.remove();
-    })
+        myArray.pop();
 
+    })
 
 });
